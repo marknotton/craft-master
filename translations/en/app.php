fc1013e6ec913e08964667a4fab6d7cdf339e715
Mark Notton <mark@yellostudio.co.uk>
2018-12-03 20:39:43 +0000

    Minor Tweaks

--- translations/en/app.php
+++ translations/en/app.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
-  "Entries" => "Pages",
-  "Entry" => "Page",
-  "New entry" => "New page",
+  "Entries"     => "Pages",
+  "Entry"       => "Page",
+  "New entry"   => "New page",
   "All entries" => "All pages",
-
 ];
